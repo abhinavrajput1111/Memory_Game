b6080004472253541f7cb2388b807a3a0ecad360
Abhinav.r.1111 <abhinav.r.1111@gmail.com>
2024-07-11 22:46:58 +0530

    updated shuffle behaviour on cards

--- script.js
+++ script.js
@@ -1,158 +1,88 @@
 let images = [
-  
-  {
-    src: "img/2.png",
-    name: "burger",
-    show: false,
-  },
- {
-    src: "img/3.png",
-    name: "peanut",
-    show: false,
-  },
-  {
-    src: "img/5.png",
-    name: "plane",
-    show: false,
-  },
-
-  {
-    src: "img/11.png",
-    name: "home",
-    show: false,
-  },
-  
-  {
-    src: "img/8.png",
-    name: "icecream",
-    show: false,
-    },
-  {
-    src: "img/1.png",
-    name: "burger",
-    show: false,
-  },
-    {
-    src: "img/14.png",
-    name: "drink",
-    show: false,
-  },
-  {
-    src: "img/9.png",
-    name: "pizza",
-    show: false,
-  },
-  
-{
-    src: "img/4.png",
-    name: "peanut",
-    show: false,
-  },
-  {
-    src: "img/12.png",
-    name: "home",
-    show: false,
-    },
-    {
-    src: "img/16.png",
-    name: "car",
-    show: false,
-  },
-  {
-    src: "img/13.png",
-    name: "drink",
-    show: false,
-  },
-  {
-    src: "img/6.png",
-    name: "plane",
-    show: false,
-    },
-  
-    {
-    src: "img/10.png",
-    name: "pizza",
-    show: false,
-  },
-
-
-  {
-    src: "img/7.png",
-    name: "icecream",
-    show: false,
-    },
-
-  {
-    src: "img/15.png",
-    name: "car",
-    show: false,
-  },
+  { src: "img/2.png", name: "burger", show: false },
+  { src: "img/3.png", name: "peanut", show: false },
+  { src: "img/5.png", name: "plane", show: false },
+  { src: "img/11.png", name: "home", show: false },
+  { src: "img/8.png", name: "icecream", show: false },
+  { src: "img/1.png", name: "burger", show: false },
+  { src: "img/14.png", name: "drink", show: false },
+  { src: "img/9.png", name: "pizza", show: false },
+  { src: "img/4.png", name: "peanut", show: false },
+  { src: "img/12.png", name: "home", show: false },
+  { src: "img/16.png", name: "car", show: false },
+  { src: "img/13.png", name: "drink", show: false },
+  { src: "img/6.png", name: "plane", show: false },
+  { src: "img/10.png", name: "pizza", show: false },
+  { src: "img/7.png", name: "icecream", show: false },
+  { src: "img/15.png", name: "car", show: false },
 ];
 
-let oneCLick = -1;
+const attemptDiv = document.querySelector(".span");
+
+let oneClick = -1;
 let second = -1;
 let score = 0;
+let attempt = 0;
 
-function renderData()
-{
-    let imagebox = document.getElementById("imagebox")
-    imagebox.innerHTML = ""
-    images.forEach((item,index) => {
-       if(item.show == true)
-       {
-        let img = document.createElement("img")
-        img.src = item.src
-        imagebox.append(img)
-    }
-       else
-       {
-        let div = document.createElement("div")
-        div.classList.add("box")
-        div.addEventListener("click", () => handleClick(index))
-        imagebox.append(div)
-       }
-
-    })
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
-function handleClick(index)
-{
-    if(oneCLick == -1)
-    {
-        images[index].show = true
-        renderData()
-        oneCLick = index;
-    }
-    else if (second == -1)
-    {
-        images[index].show = true
-        renderData()
-        second = index
-        
+let shuffledImages = shuffleArray([...images]);
 
-        setTimeout(checkBothImages,1000)
-    
+function renderData() {
+  let imagebox = document.getElementById("imagebox");
+  imagebox.innerHTML = "";
+  shuffledImages.forEach((item, index) => {
+    if (item.show == true) {
+      let img = document.createElement("img");
+      img.src = item.src;
+      imagebox.append(img);
+    } else {
+      let div = document.createElement("div");
+      div.classList.add("box");
+      div.addEventListener("click", () => handleClick(index));
+      imagebox.append(div);
     }
-   
+  });
 }
 
-function checkBothImages()
-{
-    if(images[oneCLick].name == images[second].name)
-    {
-        oneCLick = -1;
-        second = -1;
-        score += 10;
-        document.getElementById("score").innerText = score
-    }
-    else
-    {
-        images[oneCLick].show = false;
-        images[second].show = false;
-        oneCLick = -1;
-        second = -1;
-        renderData();
-    }
+function handleClick(index) {
+  if (oneClick == -1) {
+    shuffledImages[index].show = true;
+    renderData();
+    oneClick = index;
+  } else if (second == -1) {
+    shuffledImages[index].show = true;
+    renderData();
+    second = index;
+    setTimeout(checkBothImages, 1000);
+  }
+
+  attempt++;
+  attemptDiv.innerHTML = attempt;
 }
 
-renderData()
+function checkBothImages() {
+  if (shuffledImages[oneClick].name == shuffledImages[second].name) {
+    oneClick = -1;
+    second = -1;
+    score += 10;
+    document.getElementById("score").innerText = score;
+  } else {
+    shuffledImages[oneClick].show = false;
+    shuffledImages[second].show = false;
+    oneClick = -1;
+    second = -1;
+    renderData();
+  }
+}
+
+window.onload = function() {
+  shuffledImages = shuffleArray([...images]);
+  renderData();
+};
